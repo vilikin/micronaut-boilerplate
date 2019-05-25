08c0c5454e167bb82a6fa91b358f0442b5782bb1
Vili Kinnunen <vili.kinnunen1@gmail.com>
2019-05-25 20:07:32 +0300

    Update openapi definition

--- src/main/kotlin/micronaut/server/Application.kt
+++ src/main/kotlin/micronaut/server/Application.kt
@@ -2,17 +2,13 @@ package micronaut.server
 
 import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
-import io.swagger.v3.oas.annotations.info.License
 
 @OpenAPIDefinition(
   info = Info(
-    title = "Hello World",
-    version = "0.0",
-    description = "My API",
-    license = License(name = "Apache 2.0", url = "http://foo.bar"),
-    contact = Contact(url = "http://vilik.in", name = "Vili", email = "vili.kinnunen1@gmail.com")
+    title = "Micronaut Boilerplate",
+    version = "1.0",
+    description = "Micronaut Boilerplate API"
   )
 )
 object Application {
